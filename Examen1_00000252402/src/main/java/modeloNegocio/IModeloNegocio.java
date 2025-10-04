e00537cefb2dc9efe9a3962dc6d4b72f6d03da7f
DeniseGarc <aliciadenise0623@gmail.com>
2025-10-03 19:21:14 -0700

    Estructura general de los paquetes

--- Examen1_00000252402/src/main/java/modeloNegocio/IModeloNegocio.java
+++ Examen1_00000252402/src/main/java/modeloNegocio/IModeloNegocio.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package vista;
+package modeloNegocio;
 
 /**
  *
  * @author Alici
  */
-public interface IObserver {
-
+public interface IModeloNegocio {
+    
 }
